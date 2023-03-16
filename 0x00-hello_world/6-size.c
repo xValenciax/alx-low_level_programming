aba2698ccb522d927faa27f717141d1d1c5b5463
xValenciax <mamo31st@gmail.com>
2023-03-16 23:23:34 +0200

    Add size.c file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+printf("Size of a char: %lu byte(s)\n",  (unsigned long)sizeof(char));
+printf("Size of a int: %lu byte(s)\n",  (unsigned long)sizeof(int));
+printf("Size of a long: %lu byte(s)\n", (unsigned long)sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n",
+(unsigned long)sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
+return (0);
+}
