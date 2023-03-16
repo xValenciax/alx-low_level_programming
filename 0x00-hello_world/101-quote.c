a0c321fab9d73d21cf26725251d28b0a68874bb9
xValenciax <mamo31st@gmail.com>
2023-03-17 01:15:18 +0200

    Make script return 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include <unistd.h>
-#include <stdio.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 1 (failure)
  */
 
 int main(void)
 {
-char *prog = "echo";
-char *arg1 = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-execlp(prog, prog, arg1, NULL);
-return (-1);
+char *arg1 = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(0, arg1, 59);
+return (1);
 }
