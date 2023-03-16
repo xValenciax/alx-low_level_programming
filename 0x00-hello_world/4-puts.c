9856e865dc7d8aecffee2ce3206b18acf4fe59da
xValenciax <mamo31st@gmail.com>
2023-03-16 22:20:21 +0200

    Add puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main functino
+ * @void - takes no parametes
+
+ * Return: 0 to exit program
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
