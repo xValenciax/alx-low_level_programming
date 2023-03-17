52667247700d14fb006828df84d1ce52a87b0268
xValenciax <mamo31st@gmail.com>
2023-03-17 22:25:08 +0200

    Add print numbers script

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all digits less than 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit;
+
+digit = 0;
+
+while(digit < 10)
+{
+printf("%d", digit);
+digit++;
+}
+printf("\n");
+return (0);
+}
