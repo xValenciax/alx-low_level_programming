01676f552c422983ccded5a6d9fb7adc629b1637
xValenciax <mamo31st@gmail.com>
2023-03-17 21:21:04 +0200

    Fix betty warnings

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,9 +6,10 @@
 /**
  * main - Entry point
  *
- * Description: prints whether a randomly assigned variable is positive or negative
+ * Description: prints whether a randomly.
+ * assigned variable is positive or negative
  *
- * Return: Always 0 (Success) 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -17,9 +18,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if(n > 0)
-	  printf("%d is positive\n", n);
+	if (n > 0)
+	printf("%d is positive\n", n);
+	else if (n < 0)
+	printf("%d is negative\n", n);
 	else
-	  printf("%d is negative\n", n);
+	printf("%d is zero\n", n);
 	return (0);
 }
