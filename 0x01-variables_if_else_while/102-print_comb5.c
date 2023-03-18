55a0504268bcc0ec52bcaa4199a48835876c9375
xValenciax <mamo31st@gmail.com>
2023-03-18 21:51:34 +0200

    A Forced commit to satisify Mr. betty

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,37 +1,22 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
- *
- * Description: Prints all combination of 2 2-digit numbers
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int firstDig;
-int scndDig;
-int thrdDig;
-int frthDig;
+int firstDig, scndDig, thrdDig, frthDig;
 
-firstDig = 48;
-scndDig = 48;
-thrdDig = 48;
-frthDig = 49;
+firstDig = 48, scndDig = 48, thrdDig = 48, frthDig = 49;
 while (firstDig != 57 || scndDig != 57)
 {
 if (frthDig != 58)
 {
-putchar(firstDig);
-putchar(scndDig);
-putchar(' ');
-putchar(thrdDig);
-putchar(frthDig);
+putchar(firstDig), putchar(scndDig), putchar(' ');
+putchar(thrdDig), putchar(frthDig);
 if (firstDig != 57 || scndDig != 56)
-{
-putchar(',');
-putchar(' ');
-}
+putchar(','), putchar(' ');
 }
 frthDig++;
 if (frthDig > 57)
@@ -39,19 +24,11 @@ if (frthDig > 57)
 frthDig = 48;
 if (thrdDig == 57)
 {
-thrdDig = firstDig;
 if (scndDig == 57)
-{
-firstDig++;
-scndDig = 48;
-thrdDig = firstDig;
-frthDig = scndDig + 1;
-}
+firstDig++, scndDig = 48, thrdDig = firstDig;
 else
-{
 scndDig++;
-frthDig = scndDig + 1;
-}
+thrdDig = firstDig, frthDig = scndDig + 1;
 }
 else
 thrdDig++;
