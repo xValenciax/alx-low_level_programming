7befe6fa0caeb614bafc6e6ca84d9772ffb4814f
xValenciax <mamo31st@gmail.com>
2023-03-18 20:29:09 +0200

    Add print comb5 file the working one

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: Prints all digits below 10 using only putchar
+ * Description: Prints all combination of 2 2-digit numbers
  *
  * Return: Always 0 (Success)
  */
@@ -13,38 +13,49 @@ int firstDig;
 int scndDig;
 int thrdDig;
 int frthDig;
- 
+
 firstDig = 48;
 scndDig = 48;;
 thrdDig = 48;
-frthDig = 49; 
+frthDig = 49;
 
- while(firstDig < 58){
-   scndDig = 48;
-   while(scndDig < 57){
-     thrdDig = firstDig;
-     while(thrdDig < 58){
-       frthDig = scndDig + 1;
-       while(frthDig < 58){
-	 putchar(firstDig);
-	 putchar(scndDig);
-	 putchar(' ');
-	 putchar(thrdDig);
-	 putchar(frthDig);
-	 if(firstDig != 57){
-	   putchar(',');
-	   putchar(' ');
-	 }
-	 frthDig++;
-       }
-       thrdDig++;
-     }
-     scndDig++;
-     /*     frthDig = scndDig + 1;*/
-   }
-   firstDig++;
-   /*   thrdDig = firstDig + 1;*/
- }
+while(firstDig != 57 || scndDig != 57){
+putchar(firstDig);
+putchar(scndDig);
+putchar(' ');
+putchar(thrdDig);
+putchar(frthDig);
+if(firstDig != 57 || scndDig != 56){
+putchar(',');
+putchar(' ');
+}
+
+
+frthDig++;
+
+if(frthDig > 57){
+frthDig = 48;
+
+if(thrdDig == 57){
+thrdDig = firstDig;
+
+if(scndDig == 57){
+firstDig++;
+scndDig = 48;
+thrdDig = firstDig;
+frthDig = scndDig + 1;
+}
+
+else{
+scndDig++;
+frthDig = scndDig + 1;
+}
+}
+
+else
+thrdDig++;
+}
+}
 
 putchar('\n');
 return (0);
