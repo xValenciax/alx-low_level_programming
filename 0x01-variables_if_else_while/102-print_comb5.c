bed684293c8447a06bf0b812595594255f456fe1
xValenciax <mamo31st@gmail.com>
2023-03-18 20:56:09 +0200

    Fixed a bug

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,49 +14,46 @@ int scndDig;
 int thrdDig;
 int frthDig;
 
-firstDig = 48;
-scndDig = 48;;
-thrdDig = 48;
-frthDig = 49;
-
-while(firstDig != 57 || scndDig != 57){
+firstDig = 48, scndDig = 48, thrdDig = 48, frthDig = 49;
+while (firstDig != 57 || scndDig != 57)
+{
 putchar(firstDig);
 putchar(scndDig);
 putchar(' ');
 putchar(thrdDig);
 putchar(frthDig);
-if(firstDig != 57 || scndDig != 56){
+if (firstDig != 57 || scndDig != 56)
+{
 putchar(',');
 putchar(' ');
 }
-
-
 frthDig++;
-
-if(frthDig > 57){
+if (frthDig > 57)
+{
 frthDig = 48;
-
-if(thrdDig == 57){
+if (thrdDig == 57)
+{
 thrdDig = firstDig;
-
-if(scndDig == 57){
+if (scndDig == 57)
+{
 firstDig++;
 scndDig = 48;
 thrdDig = firstDig;
 frthDig = scndDig + 1;
 }
-
-else{
+else
+{
 scndDig++;
+if (scndDig == 57)
+frthDig = 48;
+else
 frthDig = scndDig + 1;
 }
 }
-
 else
 thrdDig++;
 }
 }
-
 putchar('\n');
 return (0);
 }
