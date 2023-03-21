768b2774d1d8a8d4c9f33656918b72c6f0723ff9
xValenciax <mamo31st@gmail.com>
2023-03-21 08:08:44 +0200

    Satisify betty

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,6 +13,17 @@ _putchar("_putchar\n");
 return (0);
 }
 
-void _putchar(char* str){
+/**
+ * _putchar - prints a string to standard output
+ *
+ * @str: the string to be printed
+ *
+ * Description: This function uses write system call
+ * to print the passed string to standard output
+ *
+ * Return: Always void
+ */
+void _putchar(char *str)
+{
 write(1, str, strlen(str));
 }
