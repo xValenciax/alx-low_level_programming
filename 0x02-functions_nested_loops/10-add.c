ab91fef951e4c818d520624b9a7348472cea82f7
xValenciax <mamo31st@gmail.com>
2023-03-22 00:11:38 +0200

    Betty satisfaction

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
 /**
+ * add - adds two integer numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of addition operation
  */
 
 int add(int a, int b)
 {
-  long int res = (long int)a + b;
-  return res;
+long int res = (long int)a + b;
+return (res);
 }
