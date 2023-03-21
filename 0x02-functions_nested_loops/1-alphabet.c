72e9c4bbd67d93232b68e8dffe4d80fcddfc22b9
xValenciax <mamo31st@gmail.com>
2023-03-21 20:46:54 +0200

    Configure right files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,5 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: always 0 (Success)
- */
-
-int main(void)
-{
-print_alphabet();
-return (0);
-}
-
-/**
  * print_alphabet - prints entire alphabet in lower case
  *
  * Return: always void
