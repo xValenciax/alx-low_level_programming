fc7aa3229edfa1ca5cd3c02331e3f34701120de7
xValenciax <mamo31st@gmail.com>
2023-03-22 22:50:00 +0200

    Fix betty for natural file

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,16 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * main - Entry Point
+ *
+ * Return: always 0 (Success)
  */
 
 int main(void)
 {
 int sum =  sum_three_five();
 printf("%d\n", sum);
-return 0;
+return (0);
 }
 
-
+/**
+ * sum_three_five - prints the sum of all multiples of 3 and 5
+ *
+ * Return: the sum of the numbers
+ */
 int sum_three_five(void)
 {
 int sum = 0, cnt = 1;
