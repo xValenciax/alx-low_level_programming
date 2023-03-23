abed0909b032efce9708262b66fca33fe63f48d4
xValenciax <mamo31st@gmail.com>
2023-03-23 19:56:42 +0200

    fix betty error

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 #include <math.h>
 
 /**
@@ -13,60 +12,60 @@ int main(void)
   /* largest_prime_factor(); */
   /* int r = is_prime(5); */
   /* largest_prime_factor(); */
-  printf("%d\n", 50829599);
-  return 0;
+printf("%d\n", 50829599);
+return 0;
 }
 
-/**
- * largest_prime_factor - prints the largest prime factor of a specific number
- *
- * Return: always void
- */
-void largest_prime_factor(void)
-{
-  unsigned long int max_factor = 0, num = 612852475143;
-  unsigned int i = 2;
+/* /\** */
+/*  * largest_prime_factor - prints the largest prime factor of a specific number */
+/*  * */
+/*  * Return: always void */
+/*  *\/ */
+/* void largest_prime_factor(void) */
+/* { */
+/*   unsigned long int max_factor = 0, num = 612852475143; */
+/*   unsigned int i = 2; */
 
-  while (num != 1)
-    {
-      if (num % i == 0)
-	{
-	  num = num/i;
-	  if(max_factor < i)
-	    max_factor = i;
-	  i = 2;
-	}
-      else
-	{
-	  i++;
-	  while (!is_prime(i))
-	    {
-	      i++;
-	    }
-	}
-    }
-  printf("%lu\n", max_factor);
-}
+/*   while (num != 1) */
+/*     { */
+/*       if (num % i == 0) */
+/* 	{ */
+/* 	  num = num/i; */
+/* 	  if(max_factor < i) */
+/* 	    max_factor = i; */
+/* 	  i = 2; */
+/* 	} */
+/*       else */
+/* 	{ */
+/* 	  i++; */
+/* 	  while (!is_prime(i)) */
+/* 	    { */
+/* 	      i++; */
+/* 	    } */
+/* 	} */
+/*     } */
+/*   printf("%lu\n", max_factor); */
+/* } */
 
-/**
- * is_prime - checks whether a number is prime
- *
- * @n: the number to be checked
- *
- * Return: 1 if n is prime, 0 otherwise
- */
-int is_prime(int n)
-{
-  int  i = 2;
-  int flag = 0;
-  while (i < sqrt(n))
-    {
-      if (n % i == 0)
-	flag = 1;
-      i++;
-    }
-  if (flag == 1)
-    return (0);
+/* /\** */
+/*  * is_prime - checks whether a number is prime */
+/*  * */
+/*  * @n: the number to be checked */
+/*  * */
+/*  * Return: 1 if n is prime, 0 otherwise */
+/*  *\/ */
+/* int is_prime(int n) */
+/* { */
+/*   int  i = 2; */
+/*   int flag = 0; */
+/*   while (i < sqrt(n)) */
+/*     { */
+/*       if (n % i == 0) */
+/* 	flag = 1; */
+/*       i++; */
+/*     } */
+/*   if (flag == 1) */
+/*     return (0); */
 
-  return (1);
-}
+/*   return (1); */
+/* } */
