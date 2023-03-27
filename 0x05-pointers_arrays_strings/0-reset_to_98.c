39fbad3a79a6d83f9a43b631ffd76dec7f6e99cd
xValenciax <mamo31st@gmail.com>
2023-03-27 17:44:44 +0200

    Add reset to 98 file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - modifiy the value of an integer pointer to 98
+ *
+ * @n: pointer to the integer to be modified
+ *
+ * Return: Always void
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+A readme file for the pointer_arrays_strings ALX task
