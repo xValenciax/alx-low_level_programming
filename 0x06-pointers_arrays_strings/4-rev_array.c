f203b71ed716d91a0fc7a570d01ead771b77c8fa
xValenciax <mamo31st@gmail.com>
2023-03-30 21:01:21 +0200

    Add rev array function

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,22 +1,28 @@
 #include "main.h"
 
 /**
+ * reverse_array - reverses the content of an array of ints
+ *
+ * @a: array to be reversed
+ * @n: size of the array
+ *
+ * Return: Always void
  */
 
 void reverse_array(int *a, int n)
 {
-  int left, right, tmp, tmp2;
+int left, right, tmp, tmp2;
 
-  left = 0, right = n-1;
+left = 0, right = n - 1;
 
-  while (left != right)
-    {
-      tmp = a[left];
-      tmp2 = a[right];
+while (left != right)
+{
+tmp = a[left];
+tmp2 = a[right];
 
-      a[left] = tmp2;
-      a[right] = tmp;
+a[left] = tmp2;
+a[right] = tmp;
 
-      left++, right--;
-    }
+left++, right--;
+}
 }
