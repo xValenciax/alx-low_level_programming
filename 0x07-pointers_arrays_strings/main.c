9451d2f56a55b8907c2a4d956ec82084838d198c
xValenciax <mamo31st@gmail.com>
2023-04-03 19:08:04 +0200

    Modifiy memst function

--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -27,13 +27,17 @@ void simple_print_buffer(char *buffer, unsigned int size)
         printf("\n");
 }
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    char buffer[98] = {0};
-    char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+    char buffer[98] = {0x00};
 
     simple_print_buffer(buffer, 98);
-    _memcpy(buffer + 50, buffer2, 10);
+    _memset(buffer+10, 0x01, 10);
     printf("-------------------------------------------------\n");
     simple_print_buffer(buffer, 98);    
     return (0);
