ab25b07a5ab9e9be98369a1f0471331ed1839045
xValenciax <mamo31st@gmail.com>
2023-04-05 16:54:20 +0200

    Add is palindrome recursive function

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * check_palindrome - checks for equality in a string and its reverse
+ *
+ * @l: start of string
+ * @r: end of string
+ * @s: input string
+ *
+ * Return: 1 if both are equal, 0 otherwise
+ */
+
+int check_palindrome(int l, int r, char *s)
+{
+if (*(s + l) != *(s + r))
+return (0);
+
+if (l > r)
+return (1);
+
+return (check_palindrome(l + 1, r - 1, s));
+}
+
+/**
+ * is_palindrome - checks if string reads the same reversed (palindrome string)
+ *
+ * @s: input string
+ *
+ * Return: 1 if string is palindrome, 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+int i = 0;
+
+while (*(s + i) != '\0')
+i++;
+
+i--;
+
+return (check_palindrome(0, i, s));
+}
