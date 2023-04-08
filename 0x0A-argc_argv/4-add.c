bd3f097acd572f36a54e12269b3b6ca2c66703b2
xValenciax <mamo31st@gmail.com>
2023-04-08 16:10:41 +0200

    Fix betty issues

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,8 +15,8 @@ int main(int argc, char *argv[])
 {
 int i = 1, j = 1, sum = 0, flag = 1;
 
-for(; j < argc && flag; j++)
-if(argv[j][0] >= '0' && argv[j][0] <= '9')
+for (; j < argc && flag; j++)
+if (argv[j][0] >= '0' && argv[j][0] <= '9')
 flag = 0;
 
 
@@ -28,7 +28,7 @@ return (0);
 
 for (; i < argc; i++)
 {
-for(j = 0; j < (int)strlen(argv[i]); j++)
+for (j = 0; j < (int)strlen(argv[i]); j++)
 {
 if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
 {
