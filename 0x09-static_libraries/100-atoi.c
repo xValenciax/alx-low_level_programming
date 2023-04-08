c75d264b36c2b5828aaed711cb4b550d533cf73d
xValenciax <mamo31st@gmail.com>
2023-04-08 02:54:03 +0200

    try to fix bug in code

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -12,31 +12,4 @@
 
 int _atoi(char *s)
 {
-  int digit_done = 0, res = 0, j = 1;
-  char *strt = s, *end = s, *i;
-  while (!digit_done)
-    {
-      if(*strt >= '0' && *strt <= '9')
-	digit_done = 1;
-      strt++;
-    }
-  
-  strt--;
-  end = (strt + 1);
-  
-  while ((*end >= '0' &&  *end <= '9'))
-    {
-      end++;
-    }
-
-  i = end;
-
-  while (i != (strt - 1))
-    {
-      printf("%d %d\n", res, *i);
-      res += *i - 48;
-      i--;
-      j *= 10;
-    }
-  return (res);
 }
