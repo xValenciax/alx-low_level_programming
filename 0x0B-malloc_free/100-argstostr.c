28011fa331f92d3406dd00fd58d3d12c00d93a30
xValenciax <mamo31st@gmail.com>
2023-04-11 20:26:56 +0200

    Add argstostr function

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * argstostr - concantenates all arguments passed to main
+ *
+ * @ac: number of arguments
+ * @av: array containing arguments
+ *
+ * Return: pointer to char to the newly created string
+ */
+
+char *argstostr(int ac, char **av)
+{
+char *res;
+int i = 0, j = 0, cnt = 0;
+unsigned long int len = 0;
+
+for (; i < ac; i++)
+len += strlen(av[i]);
+
+if (ac == 0 || av == NULL)
+return (NULL);
+
+res = malloc(len *sizeof(char));
+
+if (res == NULL)
+return (NULL);
+
+for (i = 0; i < ac; i++)
+{
+j = 0;
+
+while (*(av[i] + j) != '\0')
+{
+res[cnt] = *(av[i] + j);
+j++;
+cnt++;
+}
+res[cnt] = '\n';
+cnt++;
+}
+res[cnt] = '\0';
+return (res);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -11,5 +11,6 @@ char *_strdup(char *);
 char *str_concat(char *, char *);
 int **alloc_grid(int, int);
 void free_grid(int **, int);
+char *argstostr(int, char **);
 
 #endif
