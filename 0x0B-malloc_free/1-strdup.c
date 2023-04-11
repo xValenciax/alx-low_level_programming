bf78ad7257a3d1606ab129831140ac1efc452c72
xValenciax <mamo31st@gmail.com>
2023-04-11 17:01:25 +0200

    Add strdup function

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strdup - allocates space in memory and fills it with the specified string
+ *
+ * @str: string to fill the allocated space with
+ *
+ * Return: a pointer to char to the newly created space
+ */
+
+char *_strdup(char *str)
+{
+char *dup;
+int cnt = 0, i = 0;
+
+if (str == NULL)
+return (NULL);
+
+while (*(str + cnt) != '\0')
+cnt++;
+
+dup = malloc(cnt *sizeof(*dup));
+
+if (dup == NULL)
+return (NULL);
+
+for (; i < cnt; i++)
+*(dup + i) = *(str + i);
+
+return (dup);
+}
