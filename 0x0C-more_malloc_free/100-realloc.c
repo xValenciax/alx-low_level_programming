94772e21cb72157768bde3fd1961c30caf8bc9fd
xValenciax <mamo31st@gmail.com>
2023-04-13 18:50:23 +0200

    Bug fix2 realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,13 +13,13 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-int *new_ptr, *tmp = ptr;
+void *new_ptr;
 unsigned int i = 0, min = old_size < new_size ? old_size : new_size;
 
 if (old_size == new_size)
 return (ptr);
 
-if (new_size == 0 && tmp != NULL)
+if (new_size == 0 && ptr != NULL)
 {
 free(ptr);
 return (NULL);
@@ -30,16 +30,16 @@ new_ptr = malloc(new_size);
 if (new_ptr == NULL)
 return (NULL);
 
-if (tmp == NULL)
+if (ptr == NULL)
 {
-free(ptr);
+/* free(ptr); */
 return (new_ptr);
 }
 
 for (; i < min; i++)
-new_ptr[i] = *(tmp + i);
+*((char *)new_ptr + i) = *((char *)ptr + i);
 
 free(ptr);
 
-return ((void *)new_ptr);
+return (new_ptr);
 }
