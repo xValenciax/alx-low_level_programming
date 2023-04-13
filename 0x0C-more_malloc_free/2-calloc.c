5c6b68416366a666cb54d09fcdb7f5d6ecb99c17
xValenciax <mamo31st@gmail.com>
2023-04-13 16:29:05 +0200

    Bug fix in calloc function

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,16 +11,19 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-void *ptr;
-/* unsigned int i = 0; */
+char *ptr;
+unsigned int i = 0;
 
 if (nmemb == 0 || size == 0)
 return (NULL);
 
-ptr = malloc(nmemb * size);
+ptr = malloc((nmemb * size));
 
 if (ptr == NULL)
 return (NULL);
+
+for (; i < (nmemb * size); i++)
+ptr[i] = 0;
 
 return (ptr);
 }
