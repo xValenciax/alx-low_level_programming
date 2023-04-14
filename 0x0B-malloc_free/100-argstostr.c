633e987bc4570a223248877746a8471e535bd3d7
xValenciax <mamo31st@gmail.com>
2023-04-14 13:43:01 +0200

    Bug fix add one more memory unit for null terminator argstostr function

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -21,7 +21,7 @@ len += strlen(av[i]);
 if (ac == 0 || av == NULL)
 return (NULL);
 
-res = malloc(len *sizeof(char) + ac);
+res = malloc(len *sizeof(char) + ac + 1);
 
 if (res == NULL)
 return (NULL);
