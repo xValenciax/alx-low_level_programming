b76b9b5bc4b0a2f40815d0a8807da363dafabd2e
xValenciax <mamo31st@gmail.com>
2023-04-16 22:44:01 +0200

    Add atoi function

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,31 +11,34 @@
 
 int _atoi(char *s)
 {
-  int digit_done = 0, res = 0, j = 1;
-  char *strt = s, *end = s, *i = NULL;
-  while (!digit_done)
-    {
-      if(*strt >= '0' && *strt <= '9')
-	digit_done = 1;
-      strt++;
-    }
-  
-  strt--;
-  end = (strt + 1);
-  
-  while ((*end >= '0' &&  *end <= '9'))
-    {
-      end++;
-    }
+int digit_fnd = 0, j = 1, is_sent = 0, is_neg = 0, temp = 0;
+unsigned int res = 0;
+char *strt = s, *end = s;
 
-  i = end;
-
-  while (i != (strt - 1))
-    {
-      printf("%d %d\n", res, *i);
-      res += *i - 48;
-      i--;
-      j *= 10;
-    }
-  return (res);
+while (!digit_fnd)
+{
+if (((*strt >= 'a' && *strt <= 'z') || (*strt >= 'A' && *strt <= 'Z'))
+&& !is_sent)
+is_sent = 1;
+if (*strt >= '0' && *strt <= '9')
+digit_fnd = 1;
+strt++;
+}
+strt--;
+end = strt;
+while ((*end >= '0' &&  *end <= '9'))
+end++;
+end--;
+if (*(strt - 1) == '-')
+is_neg = 1;
+while (end != (strt - 1))
+{
+temp = *end - '0';
+res += (temp *j);
+j *= 10;
+end--;
+}
+if (is_neg && !is_sent)
+res *= -1;
+return (res);
 }
--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,18 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main - Entry point
+ * main - Generates a random key
  *
- * Return: Always 0 (Success)
+ * Return - always 0 (Success)
  */
 
-int main(void)
-{
-  srand(time(NULL));
-  unsigned int random_key = rand();
 
-  printf("%d\n", random_key);
+unsigned long int key_gen();
+unsigned long int calc_ckecksum(unsigned long int key);
+
+int main()
+{
+  //  printf("%lu", key_gen());
+  srand(time(NULL));
+
+  printf("%lu\n", key_gen());
   return 0;
 }
 
+unsigned long int key_gen()
+{
+  unsigned long int key = 0, keysum = 0;
+  int flag = 0, checksum = 0;
+
+  srand(time(NULL));
+  while(!flag)
+    {
+      key = (rand() % (122 - 97 + 1)) + 97;
+      keysum += key;
+      checksum = calc_ckecksum(keysum);
+      if(checksum == 2772)
+	flag = 1;
+
+      if(checksum > 2772)
+	keysum = 0;
+    }
+  return (keysum);
+}
+
+unsigned long int calc_ckecksum(unsigned long int key)
+{
+  unsigned long int sum = 0;
+
+  unsigned long int tmp = key;
+  
+  while(tmp != 0)
+    {
+      sum += tmp % 10;
+      tmp /= 10;
+    }
+
+  return (sum);
+}
