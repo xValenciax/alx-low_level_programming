83d75dc913a826e95bf6b73e9af4e0c7be245c4c
xValenciax <mamo31st@gmail.com>
2023-04-17 01:30:59 +0200

    Bug fix3 atoi function

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,17 +11,18 @@
 
 int _atoi(char *s)
 {
-int digit_fnd = 0, is_sent = 0, is_neg = 0, temp = 0, res = 0;
+unsigned int digit_fnd = 0, is_sent = 0, is_neg = 0, temp = 0, res = 0;
 unsigned int pow = 1;
 int i = 0, l, r;
 
 while (*(s + i) != '\0')
 {
-if ((*(s + i) != '-' && *(s + i) != ' ' && *(s + i) != '\t'
-&& ((*(s + i) >= 'a' && *(s + i) <= 'z') ||
-(*(s + i) >= 'A' && *(s + i) <= 'Z')))
+if (((*(s + i) >= 'a' && *(s + i) <= 'z')
+|| (*(s + i) >= 'A' && *(s + i) <= 'Z'))
 && !is_sent)
 is_sent = 1;
+if (*(s + i) == '-' && !is_neg)
+is_neg = -1;
 if (*(s + i) >= '0' && *(s + i) <= '9' && !digit_fnd)
 digit_fnd = 1, l = i;
 i++;
@@ -32,16 +33,16 @@ r = l;
 while ((*(s + r) >= '0' &&  *(s + r) <= '9'))
 r++;
 r--;
-if (*(s + l - 1) == '-')
+if (is_sent)
 is_neg = 1;
 while (r != (l - 1))
 {
-temp = *(s + r) - '0';
+temp = (*(s + r)-'0');
 res += (temp *pow);
 pow *= 10;
 r--;
 }
 if (is_neg && !is_sent)
 res *= -1;
-return (res);
+return (res *is_neg);
 }
