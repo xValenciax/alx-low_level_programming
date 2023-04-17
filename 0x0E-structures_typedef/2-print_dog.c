3de1ee2285512add93ba289064a113a44142a603
xValenciax <mamo31st@gmail.com>
2023-04-17 17:09:34 +0200

    Final bug fix for task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,12 +8,14 @@
  * Return: always void
  */
 
+#define frmt(x) ((x == NULL) ? "(nil)" : (x))
+
 void print_dog(struct dog *d)
 {
 if (!d)
 return;
 
-printf("Name: %s\n", d->name == NULL ? "nil" : d->name);
+printf("Name: %s\n", frmt(d->name));
 printf("Age: %f\n", d->age);
-printf("Owner: %s\n", d->owner == NULL ? "nil" : d->owner);
+printf("Owner: %s\n", frmt(d->owner));
 }
