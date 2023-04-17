e34e3afb9a23f992c783e0622f8287bce0e83c51
xValenciax <mamo31st@gmail.com>
2023-04-17 16:27:20 +0200

    Add dog header file for task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+this is a readme file for ALX'x structs and typedefs task
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - represents a dog
+ *
+ * @name: a string for the name of the dog
+ * @owner: a string for the name of the dog's owner
+ * @age: a float for the dog's age
+ */
+
+struct dog
+{
+char *name;
+char *owner;
+float age;
+};
+
+#endif
