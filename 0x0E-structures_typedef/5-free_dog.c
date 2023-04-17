06be7d22f1ae1893c997782943ad6da13aa90f20
xValenciax <mamo31st@gmail.com>
2023-04-17 17:59:31 +0200

    Add function free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees a varible of type struct dog
+ *
+ * @d: variable to be freed
+ *
+ * Return: Always void
+ */
+
+void free_dog(dog_t *d)
+{
+free(d->name);
+free(d->owner);
+
+free(d);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,5 +24,6 @@ typedef struct dog dog_t;
 void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *);
 dog_t *new_dog(char *, float, char *);
+void free_dog(dog_t *);
 
 #endif
