32ea04ba7578275fe0fa4d9cb558e897ecc6eb0b
xValenciax <mamo31st@gmail.com>
2023-04-17 16:38:25 +0200

    bug fix for task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,11 +7,13 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: name of the dog's owner
+ *
+ * Return: Always void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-d->name = name;
-d->age = age;
-d->owner = owner;
+(*d).name = name;
+(*d).age = age;
+(*d).owner = owner;
 }
