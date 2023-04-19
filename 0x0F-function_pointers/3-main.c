a78b0ba449d7c5d6ecaf09f405c53c2bc19d6d07
xValenciax <mamo31st@gmail.com>
2023-04-20 01:20:42 +0200

    Bug fix program simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,20 +14,21 @@ int main(int argc, char *argv[])
 int num1, num2;
 int (*operation)(int, int);
 char *op;
+
 if (argc != 4)
 printf("Error\n"), exit(98);
 
-if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*'
-&& argv[2][0] != '/' && argv[2][0] != '%')
-printf("Error\n"), exit(99);
-
-if (argv[argc - 1][0] == '0')
+if (argv[argc - 1][0] == '0' &&
+(argv[2][0] == '/' || argv[2][0] == '%'))
 printf("Error\n"), exit(100);
 
 num1 = atoi(argv[1]), num2 = atoi(argv[argc - 1]);
 op = argv[2];
 
 operation = get_op_func(op);
+
+if (!operation)
+printf("Error\n"), exit(99);
 
 printf("%d\n", operation(num1, num2));
 return (0);
