3a4a40c5fbc2c2d5b8044a1770d078b98f7ee7e0
xValenciax <mamo31st@gmail.com>
2023-04-20 15:43:47 +0200

    Bug fix1 function print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,8 +14,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 va_list numbers;
 unsigned int i = 0;
 
-if (!n)
+if (n <= 0)
+{
+printf("\n");
 return;
+}
 
 va_start(numbers, n);
 
