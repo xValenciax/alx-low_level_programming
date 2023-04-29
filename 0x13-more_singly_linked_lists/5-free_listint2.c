b40ab623676cdd2d5e57938ba471d395bbe208ff
xValenciax <mamo31st@gmail.com>
2023-04-29 13:41:50 +0300

    Bug fix task 5 in linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,12 @@
 
 void free_listint2(listint_t **head)
 {
-listint_t *curr = *head, *next;
+listint_t *curr, *next;
 
 if (head == NULL)
 return;
 
+curr = *head;
 while (curr)
 {
 next = curr->next;
