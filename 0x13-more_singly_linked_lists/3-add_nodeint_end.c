82a8a64e6fb3d667e20b513bc3ffb675addc996b
xValenciax <mamo31st@gmail.com>
2023-04-29 12:54:07 +0300

    Add task 3 in more linked list

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adds a new node to the end of a list
+ *
+ * @head: head of the list
+ * @n: value of the new node
+ *
+ * Return: pointer to the new node (Success), NULL (Failure)
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+listint_t *temp, *it;
+
+temp = malloc(sizeof(listint_t));
+
+if (!temp)
+return (NULL);
+
+temp->n = n;
+temp->next = NULL;
+if (!(*head))
+{
+*head = malloc(sizeof(listint_t));
+*head = temp;
+}
+
+else
+{
+it = *head;
+while (it && it->next)
+{
+it = it->next;
+}
+it->next = temp;
+}
+
+return (temp);
+}
