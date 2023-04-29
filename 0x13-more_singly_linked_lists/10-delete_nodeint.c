60ab4a84e120dbea7ba7681f8a91c5b506fbd70b
xValenciax <mamo31st@gmail.com>
2023-04-29 15:50:22 +0300

    Bug fix task 10 in linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,6 +1,27 @@
 #include "lists.h"
 
 /**
+ * listint_len - prints the length of a linked list
+ *
+ * @h: head of the list
+ *
+ * Return: number of nodes in a list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+int len = 0;
+
+while (h)
+{
+h = h->next;
+len++;
+}
+
+return (len);
+}
+
+/**
  * delete_nodeint_at_head - deletes head node of a list
  *
  * @head: head of the list
@@ -31,7 +52,7 @@ return (1);
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 listint_t *temp, *delNode;
-unsigned int i = 0;
+unsigned int i = 0, len;
 
 if (!head || !(*head))
 return (-1);
@@ -39,12 +60,14 @@ return (-1);
 if (index == 0)
 return (delete_nodeint_at_head(head));
 
+len = listint_len(*head);
+
+if (index > len)
+return (-1);
+
 temp = *head;
 while (i < index - 1)
 {
-if (!temp->next)
-return (-1);
-
 temp = temp->next;
 i++;
 }
