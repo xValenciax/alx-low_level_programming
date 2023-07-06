724ab0712f0e134a7cad90f1a711fad3ccf668a4
Mahmoud Mohamed <71465891+xValenciax@users.noreply.github.com>
2023-07-06 20:12:24 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - return the length of a string
- *
- * @s: string
- *
- * Return: the length of string s
- */
-
-int _strlen(const char *s)
-{
-int len;
-
-for (len = 0; *s != '\0'; len++)
-s++;
-
-return (len);
-}
-
-/**
  * binary_to_uint - converts a binary string into an unsigned integer
  * @b: binary string
  *
@@ -27,26 +9,19 @@ return (len);
 
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int result = 0, tmpProd = 1;
-int len = _strlen(b) - 1, i, j, pow = 0;
+unsigned int result = 0;
+int i;
 
 if (!b)
 return (0);
 
-for (i = len; i >= 0; i--)
+for (i = 0; b[i]; i++)
 {
 if (b[i] != '0' && b[i] != '1')
 return (0);
 
-if (b[i] == '1')
-{
-for (j = 0; j < pow; j++)
-tmpProd *= 2;
-
-result += tmpProd;
-tmpProd = 1;
-}
-pow++;
+result = result * 2 + (b[i] - '0');
+printf("result = %d\n", result);
 }
 return (result);
 }
