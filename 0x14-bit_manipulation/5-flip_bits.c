31688d1294a6960f7c1cd12fac8ffdbf795e2aad
xValenciax <mamo31st@gmail.com>
2023-07-06 11:09:03 +0300

    finished task 6

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits that needs flipping
+ * to make number 'n' equal to number 'm'
+ *
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits to be flipped
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+int no_bits = 0, i = 0;
+
+for (; i < 32; i++)
+{
+if (GET_BIT(n, i) != GET_BIT(m, i))
+no_bits++;
+}
+
+return (no_bits);
+}
