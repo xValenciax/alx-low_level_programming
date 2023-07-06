2cbec9a2c8283fd4d49f0e0efab6bec2326965e5
Mahmoud Mohamed <71465891+xValenciax@users.noreply.github.com>
2023-07-06 20:06:09 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -35,16 +35,17 @@ return (0);
 
 for (i = len; i >= 0; i--)
 {
+if (b[i] != '0' && b[i] != '1')
+return (0);
+
 if (b[i] == '1')
 {
 for (j = 0; j < pow; j++)
 tmpProd *= 2;
-result += tmpProd;
 
+result += tmpProd;
 tmpProd = 1;
 }
-else if (b[i] != '0')
-return (0);
 pow++;
 }
 return (result);
