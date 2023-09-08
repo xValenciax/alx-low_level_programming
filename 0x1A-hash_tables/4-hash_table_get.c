c069befbf6fb6a9c93a9f12391c4f419f7359ac5
Selim <mamo31st@gmail.com>
2023-09-08 23:17:30 +0300

    updates

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,24 +1,32 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_get - retrieves a value associated with a key
+ *
+ * @ht: hash table to retrieve from
+ * @key: key of the value to be retrieved
+ *
+ * Return: Null (Failure), value (Success)
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-  unsigned long int index = 0;
-  hash_node_t *curr = NULL;
+unsigned long int index = 0;
+hash_node_t *curr = NULL;
 
-  if (!key || !(*key) || !ht)
-    return (NULL);
+if (!key || !(*key) || !ht)
+return (NULL);
 
-  index = key_index((unsigned char *)key, ht->size);
+index = key_index((unsigned char *)key, ht->size);
 
-  curr = ht->array[index];
-  if (curr && strcmp(curr->key, key))
-    return (curr->value);
+curr = ht->array[index];
+if (curr && strcmp(curr->key, key))
+return (curr->value);
 
-  while (curr && strcmp(curr->key, key))
-    curr = curr->next;
+while (curr && strcmp(curr->key, key))
+curr = curr->next;
 
-  if (!curr)
-    return (NULL);
+if (!curr)
+return (NULL);
 
-  return (curr->value);
+return (curr->value);
 }
