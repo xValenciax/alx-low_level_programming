f37850fb13ae5d0d99b7431cd7790bb17df0d8dc
Selim <mamo31st@gmail.com>
2023-09-09 02:08:05 +0300

    updates

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table content
+ *
+ * @ht: hash table to print
+ *
+ * Return: always void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+unsigned int i = 0;
+hash_node_t *curr = NULL;
+
+if (!ht || ht->size == 0 || !ht->array)
+return;
+
+printf("{");
+for (; i < ht->size; i++)
+{
+curr = ht->array[i];
+while (curr)
+{
+printf("'%s': '%s', ", curr->key, curr->value);
+curr = curr->next;
+}
+}
+printf("}\n");
+}
